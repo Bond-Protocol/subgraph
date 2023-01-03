cf375be905c336c146cf275bfd9f2d7bfd394e92
spaceturtleship <spaceturtleship@protonmail.com>
2023-01-03 20:41:15 +0000

    Add extra Market fields: chainId callbackAddress capacity capacityInQuote minPrice scale

--- src/auctioneer-common.ts
+++ src/auctioneer-common.ts
@@ -4,6 +4,7 @@ import {Auctioneer} from "../generated/templates/Auctioneer/Auctioneer";
 import {loadOrAddERC20Token} from "./erc20";
 import {erc20ToBalancerPoolToken, isBalancerPool} from "./balancer-pool";
 import {erc20ToSlpPair, isLpToken} from "./slp";
+import {CHAIN_IDS} from "./chain-ids";
 
 export function createMarket(
   id: BigInt,
@@ -30,10 +31,13 @@ export function createMarket(
   let market = Market.load(id.toString());
 
   if (!market) {
+    const markets = contract.markets(id);
+
     market = new Market(id.toString());
     market.id = dataSource.network() + "_" + auctioneerName + "_" + id.toString();
     market.name = auctioneerName;
     market.network = dataSource.network();
+    market.chainId = BigInt.fromI32(CHAIN_IDS.get(dataSource.network()));
     market.auctioneer = address.toHexString();
     market.teller = contract.getTeller().toHexString();
     market.marketId = id;
@@ -48,6 +52,11 @@ export function createMarket(
     market.totalPayoutAmount = BigDecimal.fromString("0");
     market.scaleAdjustment = contract.marketScale(id);
     market.creationBlockTimestamp = timestamp;
+    market.callbackAddress = markets.getCallbackAddr().toHexString();
+    market.capacity = markets.getCapacity();
+    market.capacityInQuote = markets.getCapacityInQuote();
+    market.minPrice = markets.getMinPrice();
+    market.scale = markets.getScale();
 
     market.save();
   }
