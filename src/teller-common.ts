e15e04819bc4d30f37e769b5dd0c03eb6ced804d
spaceturtleship <spaceturtleship@protonmail.com>
2023-03-27 10:08:06 +0900

    Tidy remaining references to multiple tellers/auctioneers

--- src/teller-common.ts
+++ src/teller-common.ts
@@ -40,16 +40,12 @@ export function createBondPurchase(
   let marketId: string;
   let marketPrice: BigDecimal;
   if (
-    auctioneerAddress.toHexString().toLowerCase() == "0x007F7A58103a31109F848Df1A14F7020E1F1b28A".toLowerCase() ||
-    auctioneerAddress.toHexString().toLowerCase() == "0x007F7A6012A5e03f6F388dd9F19Fd1D754Cfc128".toLowerCase() ||
     auctioneerAddress.toHexString().toLowerCase() == "0x007F7A1cb838A872515c8ebd16bE4b14Ef43a222".toLowerCase()
   ) {
     const auctioneer = BondFixedTermCDAAbi.bind(auctioneerAddress);
     marketId = chainId + "_BondFixedTermCDA_" + id.toString();
     marketPrice = BigDecimal.fromString(auctioneer.marketPrice(id).toString());
   } else if (
-    auctioneerAddress.toHexString().toLowerCase() == "0x007FEA7A23da99F3Ce7eA34F976f32BF79A09C43".toLowerCase() ||
-    auctioneerAddress.toHexString().toLowerCase() == "0x007FEA2a31644F20b0fE18f69643890b6F878AA6".toLowerCase() ||
     auctioneerAddress.toHexString().toLowerCase() == "0x007FEA32545a39Ff558a1367BBbC1A22bc7ABEfD".toLowerCase()
   ) {
     const auctioneer1 = BondFixedExpCDAAbi.bind(auctioneerAddress);
